5325e12dfaf96b5264eb9cdcb8e7f8d28617880a
seladb <pcapplusplus@gmail.com>
2025-02-20 23:11:33 -0800

    Move legacy to `legacy` folder

--- server/linux/Log.h
+++ server/linux/Log.h
@@ -1,5 +1,5 @@
 #include "libs/AixLog/aixlog.hpp"
 
-#define TOYVPN_LOG_INFO(message) LOG(INFO) << COLOR(green) << message << COLOR(none) << "\n";
-#define TOYVPN_LOG_ERROR(message) LOG(ERROR) << COLOR(red) << message << COLOR(none) << "\n";
-#define TOYVPN_LOG_DEBUG(message) LOG(DEBUG) << message << "\n";
+#define TOYVPN_LOG_INFO(message) LOG(INFO) << COLOR(green) << message << COLOR(none) << "\n"
+#define TOYVPN_LOG_ERROR(message) LOG(ERROR) << COLOR(red) << message << COLOR(none) << "\n"
+#define TOYVPN_LOG_DEBUG(message) LOG(DEBUG) << message << "\n"
