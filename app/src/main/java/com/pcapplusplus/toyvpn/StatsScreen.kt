6d732a437ea77baa9198e5f38b03ea6a4f206133
seladb <pcapplusplus@gmail.com>
2025-03-01 23:38:25 -0800

    Add logs

--- app/src/main/java/com/pcapplusplus/toyvpn/StatsScreen.kt
+++ app/src/main/java/com/pcapplusplus/toyvpn/StatsScreen.kt
@@ -2,6 +2,7 @@ package com.pcapplusplus.toyvpn
 
 import android.content.Intent
 import android.net.Uri
+import android.util.Log
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
@@ -83,7 +84,9 @@ fun StatsScreen(
     }
 
     val onDisconnectClicked: () -> Unit = {
+        Log.e("StatsScreenLog", "Before viewModel.disconnectVpn()")
         viewModel.disconnectVpn()
+        Log.e("StatsScreenLog", "After viewModel.disconnectVpn()")
     }
 
     Column(
