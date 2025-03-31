8e04b78fb54d1ccfb20d4d9c85b1ee4e24e0d671
seladb <pcapplusplus@gmail.com>
2025-03-30 23:30:39 -0700

    Upgrade to SDK 36

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
     defaultConfig {
         applicationId = "com.pcapplusplus.toyvpn"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 36
         versionCode = 1
         versionName = "1.0"
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
